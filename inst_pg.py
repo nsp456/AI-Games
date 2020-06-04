029e60f0de883339055cb931dc4fff1544ce3ac2
nsp456 <61820335+nsp456@users.noreply.github.com>
2020-06-04 15:58:20 +0530

    Update inst_pg.py

--- inst_pg.py
+++ inst_pg.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.title("Instructions for AI Learns")
 T = tk.Text(root, height=10, width=60)
 T.pack()
 T.insert(tk.END, "\nThis AI game will first analyse every possible path to \nvictory and then it's speed of achieving victory will \nkeep on increasing until it reaches a point where it\nkeeps winning in every trial. \nGame will begin as soon as you close this Window\n\nENJOY !!!")
