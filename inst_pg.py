6a319f721433a4031533c6576bdac8ea95776d07
nsp456 <61820335+nsp456@users.noreply.github.com>
2020-06-04 17:43:12 +0530

    Update inst_pg.py

--- inst_pg.py
+++ inst_pg.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 root = tk.Tk()
 root.title("Instructions for AI Learns")
-T = tk.Text(root, height=10, width=60)
+T = tk.Text(root, height=11, width=60)
 T.pack()
-T.insert(tk.END, "\nThis AI game will first analyse every possible path to \nvictory and then it's speed of achieving victory will \nkeep on increasing until it reaches a point where it\nkeeps winning in every trial. \nGame will begin as soon as you close this Window\n\nENJOY !!!")
+T.insert(tk.END, "\nIn the game, Top left corner shows the trial number.\nAt first the player moves randomly and it gets feedback \ndepending whether it gets closer or moves further away \nfrom the goal, and based on that it makes decision to \nget more positive feedback. If the player goes out of \nboundary, he dies and next trial begins. \nGame will begin as soon as you close this Window\n\nENJOY !!!")
 tk.mainloop()
